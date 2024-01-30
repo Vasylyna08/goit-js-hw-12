8fd4a7c5ee2eebb194cb7584a31b788265c0dd13
Vasylyna08 <141525962+Vasylyna08@users.noreply.github.com>
2024-01-30 05:06:29 +0200

    Index.html + main.js + styles.css

--- src/main.js
+++ src/main.js
@@ -4,54 +4,114 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '41985459-07284690ed1bbc3dd300f203e';
 
 const formEl = document.querySelector('.form');
 const galleryEl = document.querySelector('.gallery');
 const loaderEl = document.querySelector('.loader');
+const loadButtonEl = document.querySelector('.load-button');
 
-loaderEl.style.display = 'none';
+const hiddenClass = 'is-hidden';
 
-async function fetchImage(imageName) {
-  const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  return response.json();
-}
+let query = '';
+let page = 1;
+let maxPage = 0;
+
+const refreshGallery = new SimpleLightbox('.gallery-item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 formEl.addEventListener('submit', handleSearch);
 
 async function handleSearch(event) {
   event.preventDefault();
-  loaderEl.style.display = 'block';
+  galleryEl.innerHTML = '';
+  page = 1;
+  loaderEl.classList.remove(hiddenClass);
   const form = event.currentTarget;
-  const query = form.elements.query.value;
+  query = form.elements.query.value;
   if (!query) {
+    loadButtonEl.classList.add(hiddenClass);
     onFetchError();
     return;
   }
   try {
     const data = await fetchImage(query);
-    loaderEl.style.display = 'none';
-    if (!data.hits.length) {
+    maxPage = Math.ceil(data.totalHits / 40);
+    createMarkup(data);
+
+    if (data.hits.length > 0 && data.hits.length !== data.totalHits) {
+      loadButtonEl.classList.remove(hiddenClass);
+      loadButtonEl.addEventListener('click', handleLoadMore);
+    } else if (!data.hits.length) {
+      loadButtonEl.classList.add(hiddenClass);
       onFetchError();
+    } else {
+      loadButtonEl.classList.add(hiddenClass);
     }
-    galleryEl.innerHTML = createMarkup(data.hits);
-    const refreshGallery = new SimpleLightbox('.gallery-item a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
-    refreshGallery.refresh();
   } catch (error) {
     onFetchError(error);
   } finally {
+    loaderEl.classList.add(hiddenClass);
     form.reset();
   }
 }
 
-function createMarkup(arr) {
-  return arr
+async function fetchImage(imageName, page = 1) {
+  try {
+    const response = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: API_KEY,
+        q: imageName,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: 40,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    onFetchError(error);
+  }
+}
+
+async function handleLoadMore() {
+  page += 1;
+  scrollPage();
+  loadButtonEl.disabled = true;
+  loaderEl.classList.remove(hiddenClass);
+
+  try {
+    const data = await fetchImage(query, page);
+    createMarkup(data);
+  } catch (error) {
+    onFetchError(error);
+  } finally {
+    loadButtonEl.disabled = false;
+    loaderEl.classList.add(hiddenClass);
+
+    if (page === maxPage) {
+      loadButtonEl.classList.add(hiddenClass);
+      loadButtonEl.removeEventListener('click', handleLoadMore);
+      iziToast.show({
+        title: 'Error',
+        messageColor: '#fff',
+        messageSize: '20px',
+        backgroundColor: '#EF4040',
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'bottomCenter',
+      });
+    }
+  }
+}
+
+function createMarkup(data) {
+  const markup = data.hits
     .map(
       ({
         webformatURL,
@@ -75,6 +135,18 @@ function createMarkup(arr) {
 </li>`
     )
     .join('');
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  refreshGallery.refresh();
+}
+
+function scrollPage() {
+  const liEl = document.querySelector('.gallery-item');
+  const twoHeight = liEl.getBoundingClientRect().height * 2;
+
+  window.scrollBy({
+    top: twoHeight,
+    behavior: 'smooth',
+  });
 }
 
 function onFetchError(error) {
